c18fc155a23e4ce54439082ab92ddf14108b3624
dullacodes5853 <abdullahgilani5853@gmail.com@gmail.com>
2023-03-27 07:44:33 -0400

    yes

--- routes/routes.js
+++ routes/routes.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 module.exports = function (app) {
 
     app.get('/', function(req, res) {
-        res.sendFile(path.join(__dirname, '/../public/main.html'));
+        res.sendFile(path.join(__dirname, '/../public/index.html'));
     });
 
     app.get('/notes', function(req, res) {
